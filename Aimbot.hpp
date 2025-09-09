879080b494b32c4597664f718968cc8db0fee0e7
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-09 08:10:38 +0200

    Create Aimbot.hpp

---
+++ Aimbot.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <vector>
+#include "../structs.hpp"       // Vector2, Vector3, Entity
+
+void Aimbot(const std::vector<Entity>& ents, const Vector3& localPos, int localTeam);
