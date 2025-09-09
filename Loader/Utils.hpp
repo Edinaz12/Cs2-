74adc0baa9fec68a74d149a13b4900bc0582bdba
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-09 10:44:44 +0200

    Update Utils.hpp

--- Loader/Utils.hpp
+++ Loader/Utils.hpp
@@ -1,7 +1,9 @@
 #pragma once
+#include <Windows.h>
+#include <vector>
 #include <string>
 
 namespace Utils {
-    DWORD FindProcessId(const std::string& processName);
-    bool VerifyAuth(const std::string& url);
+    DWORD FindProcessId(const std::wstring& name);
+    bool ReadFileBytes(const std::string& path, std::vector<BYTE>& out);
 }
