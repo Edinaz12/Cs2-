d271210423d1feb8b8da39a9071a2e8a4a3493bf
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-09 08:17:36 +0200

    ESP.hpp

--- ESP.hpp
+++ ESP.hpp
@@ -1,16 +1,6 @@
 #pragma once
 #include <vector>
-#include <Windows.h>
-#include "../memory.hpp"     // Entity, Vector3
-#include "../globals.hpp"    // Vector2
-#include "../config.hpp"     // CheatConfig::Get()
+#include "../globals.hpp"
+#include "../memory.hpp"
 
-void DrawESP(const std::vector<Entity>& ents, const Vector3& localPos, const float* mat);
-
-// Optional: eigene Drawing-Wrappers (für später austauschbar auf ImGui)
-void DrawBox(int x, int y, int w, int h, COLORREF color);
-void DrawTextOut(int x, int y, const char* text, COLORREF color);
-void DrawCircle(int x, int y, int radius, COLORREF color);
-void DrawHealthBar(int x, int y, int width, int height, int hp);
-void BeginDraw();
-void EndDraw();
+void DrawESP(const std::vector<Entity>& ents, const Vector3& localPos, const float* viewMatrix);
