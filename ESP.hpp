a90d72eb5f6e526b839ed42359a9b1c2052ce03e
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-09 15:35:38 +0200

    Update ESP.hpp

--- ESP.hpp
+++ ESP.hpp
@@ -1,6 +1,6 @@
 #pragma once
 #include <vector>
-#include "../globals.hpp"
-#include "../memory.hpp"
+#include "globals.hpp"
+#include "memory.hpp"
 
 void DrawESP(const std::vector<Entity>& ents, const Vector3& localPos, const float* viewMatrix);
