369e3da3dfc308c3ea4cf1c039350400f181b4e3
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-09 09:42:39 +0200

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -1,80 +1,40 @@
 #include "menu.hpp"
-#include "../config/CheatConfig.hpp"
-#include "imgui/imgui.h"
-#include <Windows.h>
+#include "../config/config.hpp"  // CheatConfig
+
+#include "imgui.h"
 
 namespace Menu {
-
     void Render() {
-        // INSERT-Toggle
-        static bool prevInsert = false;
-        bool curInsert = GetAsyncKeyState(VK_INSERT) & 1;
-        if (curInsert && !prevInsert)
-            bShowMenu = !bShowMenu;
-        prevInsert = curInsert;
+        static bool open = true;
+        if (!open) return;
 
-        if (!bShowMenu)
-            return;
-
-        ImGui::Begin("Edinaz Solutions", nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+        ImGui::Begin("Edinaz Solutions", &open, ImGuiWindowFlags_AlwaysAutoResize);
 
         ImGui::Text("Visuals");
         ImGui::Separator();
+        ImGui::Checkbox("ESP", CheatConfig::Get().GetPtr<bool>("esp"));
+        ImGui::Checkbox("RCS", CheatConfig::Get().GetPtr<bool>("rcs"));
 
-        bool esp = CheatConfig::Get().Get<bool>("esp", true);
-        if (ImGui::Checkbox("ESP", &esp)) {
-            CheatConfig::Get().Set("esp", esp);
-        }
-
-        bool hp = CheatConfig::Get().Get<bool>("hp_enabled", true);
-        if (ImGui::Checkbox("Health Bar", &hp)) {
-            CheatConfig::Get().Set("hp_enabled", hp);
-        }
-
-        bool dist = CheatConfig::Get().Get<bool>("distance", true);
-        if (ImGui::Checkbox("Distance Text", &dist)) {
-            CheatConfig::Get().Set("distance", dist);
-        }
-
-        bool head = CheatConfig::Get().Get<bool>("head_enabled", true);
-        if (ImGui::Checkbox("Head ESP", &head)) {
-            CheatConfig::Get().Set("head_enabled", head);
-        }
-
-        ImGui::Text("Combat");
+        ImGui::Spacing();
+        ImGui::Text("Aimbot");
         ImGui::Separator();
+        ImGui::Checkbox("Enable Aimbot", CheatConfig::Get().GetPtr<bool>("aimbot"));
+        ImGui::SliderFloat("FOV", CheatConfig::Get().GetPtr<float>("fov"), 1.0f, 50.0f, "%.1f");
+        ImGui::SliderFloat("Smoothing", CheatConfig::Get().GetPtr<float>("smoothing"), 1.0f, 20.0f, "%.1f");
 
-        bool aimbot = CheatConfig::Get().Get<bool>("aimbot", true);
-        if (ImGui::Checkbox("Aimbot", &aimbot)) {
-            CheatConfig::Get().Set("aimbot", aimbot);
-        }
-
-        float fov = CheatConfig::Get().Get<float>("fov", 10.0f);
-        if (ImGui::SliderFloat("Aimbot FOV", &fov, 1.0f, 50.0f)) {
-            CheatConfig::Get().Set("fov", fov);
-        }
-
-        float smoothing = CheatConfig::Get().Get<float>("smoothing", 3.0f);
-        if (ImGui::SliderFloat("Smoothing", &smoothing, 1.0f, 20.0f)) {
-            CheatConfig::Get().Set("smoothing", smoothing);
-        }
-
-        bool trigger = CheatConfig::Get().Get<bool>("triggerbot", true);
-        if (ImGui::Checkbox("Triggerbot", &trigger)) {
-            CheatConfig::Get().Set("triggerbot", trigger);
-        }
-
-        int delay = CheatConfig::Get().Get<int>("trigger_delay", 10);
-        if (ImGui::SliderInt("Trigger Delay", &delay, 0, 100)) {
-            CheatConfig::Get().Set("trigger_delay", delay);
-        }
-
-        bool rcs = CheatConfig::Get().Get<bool>("rcs", true);
-        if (ImGui::Checkbox("RCS", &rcs)) {
-            CheatConfig::Get().Set("rcs", rcs);
-        }
+        ImGui::Spacing();
+        ImGui::Text("Triggerbot");
+        ImGui::Separator();
+        ImGui::Checkbox("Enable Triggerbot", CheatConfig::Get().GetPtr<bool>("triggerbot"));
+        ImGui::SliderInt("Delay (ms)", CheatConfig::Get().GetPtr<int>("trigger_delay"), 1, 100);
+        ImGui::Combo("Hitbox", CheatConfig::Get().GetPtr<std::string>("trigger_hitbox"),
+            [](void* data, int idx, const char** out_text) {
+                static const char* items[] = { "any", "head", "chest" };
+                if (idx < 0 || idx >= 3) return false;
+                *out_text = items[idx];
+                return true;
+            }, nullptr, 3);
 
         ImGui::End();
     }
-
 }
