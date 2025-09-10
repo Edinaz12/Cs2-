62030c537835da5502f4c43c386b755330c129f9
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-10 07:53:58 +0200

    Update Aimbot.hpp

--- Aimbot.hpp
+++ Aimbot.hpp
@@ -2,6 +2,12 @@
 #include <vector>
 #include "globals.hpp"
 
-struct Entity;
+struct Entity {
+    int id;
+    int health;
+    int team;
+    Vector3 pos;
+    bool visible;
+};
 
 void Aimbot(const std::vector<Entity>& ents, const Vector3& localPos, int localTeam);
