bd6c1d07969a1a96471d7687d7ffbac6b2aaefc6
Edinaz12 <166111120+Edinaz12@users.noreply.github.com>
2025-09-10 07:52:43 +0200

    Update Aimbot.hpp

--- Aimbot.hpp
+++ Aimbot.hpp
@@ -1,6 +1,7 @@
 #pragma once
-
 #include <vector>
-#include "../structs.hpp"       // Vector2, Vector3, Entity
+#include "globals.hpp"
+
+struct Entity;
 
 void Aimbot(const std::vector<Entity>& ents, const Vector3& localPos, int localTeam);
